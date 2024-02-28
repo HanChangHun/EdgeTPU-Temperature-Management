e3d9d4687680000e96eaffc3381473d8280a491e
HanChnagHun <ehwjs1914@naver.com>
2024-02-28 14:07:47 +0900

    Update CDB_read_proc_temperature.sh script to enable/disable fan

--- src/CDB_read_proc_temperature.sh
+++ src/CDB_read_proc_temperature.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# 기본 측정 시간을 설정합니다. 인자가 제공되지 않을 경우 사용됩니다.
-DUR=10
-PORT=22 # 기본 포트를 22로 설정
-USER=""
+USER="mendel" # Set default user to "mendel"
 HOST=""
+PORT=22 # Set default port to 22
+DUR=10
 
 # 옵션과 인자를 처리합니다.
-while getopts "t:p:u:h:" opt; do
+while getopts "u:h:p:t:" opt; do
     case $opt in
-    t) DUR=$OPTARG ;;
-    p) PORT=$OPTARG ;;
     u) USER=$OPTARG ;;
     h) HOST=$OPTARG ;;
+    p) PORT=$OPTARG ;;
+    t) DUR=$OPTARG ;;
     \?)
         echo "Invalid option: -$OPTARG" >&2
         exit 1
@@ -21,7 +20,7 @@ while getopts "t:p:u:h:" opt; do
 done
 
 if [ -z "$USER" ] || [ -z "$HOST" ]; then
-    echo "Usage: $0 -t <DUR> -p <PORT> -u <USER> -h <HOST>"
+    echo "Usage: $0 -u <USER> -h <HOST> -p <PORT> -t <DUR>"
     exit 1
 fi
 
@@ -32,7 +31,11 @@ FILE_NAME="result/$(date +%Y%m%d_%H%M%S)/proc_temp_data.csv"
 scp -P $PORT ./src/CDB_read_proc_temperature.py \
     $USER@$HOST:/tmp/CDB_read_proc_temperature.py
 
+./src/CDB_disable_fan.sh -u $USER -h $HOST -p $PORT
+
 ssh -p $PORT $USER@$HOST "python3 /tmp/CDB_read_proc_temperature.py -t $DUR"
+
+./src/CDB_enable_fan.sh -u $USER -h $HOST -p $PORT
 
 ssh -p $PORT $USER@$HOST \
     "ls -t /tmp/result/proc_temp_data_*.csv | head -n 1" |
